7de9cbac825c93dec7928cfefb4a5875717c615d
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2019-10-14 19:35:45 +0300

    addin problem statement and citation information

--- 04-MovieSchedulingProblem/movieSchedulingProblem.cpp
+++ 04-MovieSchedulingProblem/movieSchedulingProblem.cpp
@@ -1,6 +1,20 @@
 // author : Mohammed S. Yaseen
 // date   : 14/10/2019
 
+// ********* PROBLEM *********
+// Input: A set I of n intervals on the line.
+// Output: What is the largest subset of mutually non-overlapping intervals which can
+// be selected from I?
+
+// ********* SOURCE  *********
+// Title        : movie Scheduling Problem
+// Author       : Steven S. Skiena
+// Date         : 2008
+// Type         : peudo code
+// Version      : unknown
+// Availability : The algorithm design manual book
+// ***************************
+
 #include <iostream>
 using namespace std;
 
