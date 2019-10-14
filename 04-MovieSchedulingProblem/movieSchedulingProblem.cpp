d6c4a840d3a2fe1c6303d1b4a0fa218807dd9c4c
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2019-10-14 19:54:19 +0300

    add the vector intervals and make sure it works

--- 04-MovieSchedulingProblem/movieSchedulingProblem.cpp
+++ 04-MovieSchedulingProblem/movieSchedulingProblem.cpp
@@ -16,14 +16,18 @@
 // ***************************
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void earliestJobFirst(){
+  vector<pair<int,int>> intervals{make_pair(5,8), make_pair(3,8), make_pair(6,12), make_pair(13,25), make_pair(15,34), make_pair(22,38) };
   
+  cout<< intervals.front().first<<" , ";
+  cout<< intervals.front().second;
 }
 
 int main(){
 
-  earlierJobFirst();
+  earliestJobFirst();
   return 0;
 }
