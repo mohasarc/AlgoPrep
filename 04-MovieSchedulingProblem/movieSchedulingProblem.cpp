5f20723c902d158990bb83c49ec9ef3aa3d7838c
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2019-10-14 19:27:57 +0300

    initial commit adding author info and first function name

---
+++ 04-MovieSchedulingProblem/movieSchedulingProblem.cpp
@@ -0,0 +1,15 @@
+// author : Mohammed S. Yaseen
+// date   : 14/10/2019
+
+#include <iostream>
+using namespace std;
+
+void earliestJobFirst(){
+  
+}
+
+int main(){
+
+  earlierJobFirst();
+  return 0;
+}
