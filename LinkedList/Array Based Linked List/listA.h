806d6e1993046201fd8f5a8a507fd84dff4f094a
Mohammed Yaseen <Moha.98.1900@gmail.com>
2019-12-10 09:01:43 +0300

    finished debugging and added main fcn to run

--- LinkedList/Array Based Linked List/listA.h
+++ LinkedList/Array Based Linked List/listA.h
@@ -1,7 +1,7 @@
 #ifndef __LISTA_H
 #define __LISTA_H
 
-const int max = 500;
+const int maxVal = 500;
 typedef int ListItemType;
 
 class list
@@ -14,15 +14,12 @@ public:
 	bool insert(int index, ListItemType newItem);
 	bool remove(int index);
 private:
-	ListItemType items[max];
+	ListItemType items[maxVal];
 	int size;
 	int translate(int index) const;
 	// item at from would become empty and item at
 	// to would be populated with the (to - 1) item
  	bool shiftItems(int from, int to);
 };
-
-
-
 
 #endif
