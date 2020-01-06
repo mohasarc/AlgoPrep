2fc7071a3f32f4b17068815cbaae5570d361a4ca
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-01-06 13:35:27 +0300

    deleting the directories with old names

--- AbstractList/run.cpp
+++ AbstractList/run.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include "listA.h"
+// #include "ArrayBasedADTList/listA.h"
+#include "PointerBasedADTList/listP.h"
 using namespace std;
 
 int main(){
 
-	list* anlist = new list();
-
+	// list* anlist = new list();
+	List* anlist = new List();
 	cout<< anlist->getLength()<<endl;
 	cout<< anlist->isEmpty()<<endl;
+
+	cout<<"here";
 
 	anlist->insert(1, 12);
 	anlist->insert(2, 24);
 	anlist->insert(2, 15);
 	anlist->insert(4, 35);
 	anlist->insert(1, 17);
+
+	cout << "inserted";
+
 	anlist->remove(3);
 
 	int a, b, c, d, e;
