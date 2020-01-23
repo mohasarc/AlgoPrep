de23a8e39522feec658d910d5a4d4958e7b54309
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-01-23 21:17:50 +0300

    change order of examples

--- Longest_Consecutive_Increasing_Branch/Longest_Consecutive_Increasing_Branch.cpp
+++ Longest_Consecutive_Increasing_Branch/Longest_Consecutive_Increasing_Branch.cpp
@@ -62,35 +62,7 @@ int longestPath(TreeNode* root){
 }
 
 int main(){
-/*              5
-              /   \
-             2     7
-            / \   / \\
-           1   3 6    8
-*/
-    TreeNode* root2 = new TreeNode;
-    root2->data = 5;
-    root2->left = new TreeNode;
-    root2->left->data = 2;
-    root2->right = new TreeNode;
-    root2->right->data = 7;
-    root2->left->left = new TreeNode;
-    root2->left->left->data = 1;
-    root2->left->left->left = NULL;
-    root2->left->left->right = NULL;
-    root2->left->right = new TreeNode;
-    root2->left->right->data = 3;
-    root2->left->right->left = NULL;
-    root2->left->right->right = NULL;
-    root2->right->left = new TreeNode;
-    root2->right->left->data = 4; 
-    root2->right->left->left = NULL;
-    root2->right->left->right = NULL;
-    root2->right->right = new TreeNode;
-    root2->right->right->data = 8;
-    root2->right->right->left = NULL;
-    root2->right->right->right = NULL;
-
+    
 /*              5
               /   \
              2     7
@@ -124,6 +96,35 @@ int main(){
     root->right->right->right->data = 9;
     root->right->right->right->left = NULL;
     root->right->right->right->right = NULL;
+
+/*              5
+              /   \
+             2     7
+            / \   / \\
+           1   3 6    8
+*/
+    TreeNode* root2 = new TreeNode;
+    root2->data = 5;
+    root2->left = new TreeNode;
+    root2->left->data = 2;
+    root2->right = new TreeNode;
+    root2->right->data = 7;
+    root2->left->left = new TreeNode;
+    root2->left->left->data = 1;
+    root2->left->left->left = NULL;
+    root2->left->left->right = NULL;
+    root2->left->right = new TreeNode;
+    root2->left->right->data = 3;
+    root2->left->right->left = NULL;
+    root2->left->right->right = NULL;
+    root2->right->left = new TreeNode;
+    root2->right->left->data = 4; 
+    root2->right->left->left = NULL;
+    root2->right->left->right = NULL;
+    root2->right->right = new TreeNode;
+    root2->right->right->data = 8;
+    root2->right->right->left = NULL;
+    root2->right->right->right = NULL;
 
     cout<< longestPath(root2)<<endl;
     cout<< longestPath(root);
