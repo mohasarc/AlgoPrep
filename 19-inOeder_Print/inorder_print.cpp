5514c305165eebd2e0bfb799fa4f4091248c097d
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-01-29 13:46:11 +0300

    tiny edits

--- 19-inOeder_Print/inorder_print.cpp
+++ 19-inOeder_Print/inorder_print.cpp
@@ -23,9 +23,9 @@ struct TreeNode{
 };
 
 void printInOrder(TreeNode* root){
-    if(root == NULL)
+    if(!root)
         return;
-    
+
     if(root->left)
         printInOrder(root->left);
 
@@ -35,37 +35,37 @@ void printInOrder(TreeNode* root){
         printInOrder(root->right);
 }
 
-int main(){
-/*              5
-              /   \
-             2     7
-            / \   / \
-           1   3 6   8
-*/
-    TreeNode* root = new TreeNode;
-    root->data = 5;
-    root->left = new TreeNode;
-    root->left->data = 2;
-    root->right = new TreeNode;
-    root->right->data = 7;
-    root->left->left = new TreeNode;
-    root->left->left->data = 1;
-    root->left->left->left = NULL;
-    root->left->left->right = NULL;
-    root->left->right = new TreeNode;
-    root->left->right->data = 3;
-    root->left->right->left = NULL;
-    root->left->right->right = NULL;
-    root->right->left = new TreeNode;
-    root->right->left->data = 6;
-    root->right->left->left = NULL;
-    root->right->left->right = NULL;
-    root->right->right = new TreeNode;
-    root->right->right->data = 8;
-    root->right->right->left = NULL;
-    root->right->right->right = NULL;
+// int main(){
+// /*              5
+//               /   \
+//              2     7
+//             / \   / \
+//            1   3 6   8
+// */
+//     TreeNode* root = new TreeNode;
+//     root->data = 5;
+//     root->left = new TreeNode;
+//     root->left->data = 2;
+//     root->right = new TreeNode;
+//     root->right->data = 7;
+//     root->left->left = new TreeNode;
+//     root->left->left->data = 1;
+//     root->left->left->left = NULL;
+//     root->left->left->right = NULL;
+//     root->left->right = new TreeNode;
+//     root->left->right->data = 3;
+//     root->left->right->left = NULL;
+//     root->left->right->right = NULL;
+//     root->right->left = new TreeNode;
+//     root->right->left->data = 6;
+//     root->right->left->left = NULL;
+//     root->right->left->right = NULL;
+//     root->right->right = new TreeNode;
+//     root->right->right->data = 8;
+//     root->right->right->left = NULL;
+//     root->right->right->right = NULL;
 
-    printInOrder(root);
+//     printInOrder(root);
 
-    return 0;
-}
+//     return 0;
+// }
