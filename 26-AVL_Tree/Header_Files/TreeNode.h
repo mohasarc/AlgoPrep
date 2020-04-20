8dfd1f8969329233d93d69d4ced17bc94a695fd5
Mohammed Yaseen <Moha.98.1900@gmail.com>
2020-04-20 13:11:01 +0300

    implemented tree node

---
+++ 26-AVL_Tree/Header_Files/TreeNode.h
@@ -0,0 +1,36 @@
+// Author: Mohammed S. Yaseen
+// Built upon the code from 25-2_3_Search_Tree/Header_Files/TriNode.h
+
+#ifndef _TRI_NODE
+#define _TRI_NODE
+#include <iostream>
+
+template < class ItemType>
+class TreeNode
+{
+private :
+ItemType item; // Data portion
+TreeNode<ItemType>* leftChildPtr; // Left-child pointer
+TreeNode<ItemType>* rightChildPtr; // Right-child pointer
+int height;
+
+public :
+TreeNode();
+TreeNode( const ItemType& anItem);
+TreeNode( const ItemType& anItem, TreeNode<ItemType>* leftPtr, TreeNode<ItemType>* rightPtr);
+bool isLeaf() const ;
+ItemType getItem() const ;
+void setItem( const ItemType& anItem);
+TreeNode<ItemType>* getLeftChildPtr() const ;
+TreeNode<ItemType>* getRightChildPtr() const ;
+void setLeftChildPtr(TreeNode<ItemType>* leftPtr);
+void setRightChildPtr(TreeNode<ItemType>* rightPtr);
+int getHeight() const ;
+void setHeight(const int h);
+}; // end TriNode
+// instantiating needed classes 
+// not to get a linking time error (undefined reference to)
+// read more : https://bytefreaks.net/programming-2/c/c-undefined-reference-to-templated-class-function
+template class TreeNode<int>;
+// #include "TriNode"
+#endif
